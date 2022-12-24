f75101ab259e75bd5bfb66bc85105738203249fc
Simone.Marocchi <simone.marocchi@kantar.com>
2022-12-24 19:26:00 +0100

    chore: invest_prospect plan

--- prophet/invest_prospect.py
+++ prophet/invest_prospect.py
@@ -3,15 +3,13 @@ Leggi i dati
 
 fai il training su un anno di dati
 
-
-
 Dati Input:
 
-- tabella dati hourly
 - dizionario parametri per il modello di prophet
 
 Parametri Input:
 
+- tabella real_datasources dati hourly ( con le colonne ds and y ? )
 - definizione portafoglio iniziale
 - decidi data di inizio predizione
 - decidi data di fine predizione
@@ -25,10 +23,45 @@ Output:
 - pandas con:
   - predizioni delle varie giornate
   - valori reali delle varie giornate
-  - andamento del portafoglio nelle varie giornate
+  - andamento del portafoglio nelle varie giornate, partendo dal valore iniziale
 
 Esempio:
 
+class InvestProspectSimulator(real_datasources=tabella_ds_y)
 
+
+
+internal method: def train_model(restrict_interval_data, prophet_parameters) -> prophet_model_ready
+
+internal method: def predict_model(prophet_model_ready, original_data, future) -> original_data+future_prediction, 
+fraction of table ready
+
+
+
+external method: def ProspectSimulation(
+  init_wallet_money,
+  init_wallet_bitcoin,
+  init_date_pred,
+  end_date_pred,
+  trade_interval,
+  invest_perc,
+  tax_perc,
+  lookup_window_span
+  )
+
+
+separazione dati
+
+
+
+
+
+
+
+prophet_input_dict={
+
+}
+
+invest_model=
 
 """
