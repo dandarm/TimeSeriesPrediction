ad2eede4dd1b98273fad1d16378c24be32db9d72
Simone.Marocchi <simone.marocchi@kantar.com>
2022-12-29 23:37:23 +0100

    chore: skeleton backtest_finance.py

--- prophet_invest/nsp_prophet/backtest_finance.py
+++ prophet_invest/nsp_prophet/backtest_finance.py
@@ -3,50 +3,67 @@ import numpy as np
 
 class BacktestFinance():
     
-    def __init__(self, original_series, initial_wallet, fees_perc, 
-    fees_fixed, forecast):
+    def __init__(
+        self, df, start_date_pred, end_date_pred,
+        interval_pred, initial_wallet, initial_stock, invest_perc, fees_perc, 
+        fees_fixed):
         
-        self.original_series = original_series
-        self.tempo = np.arange(len(original_series), dtype=int)
+        self.df = df
+        self.start_date_pred = start_date_pred
+        self.end_date_pred = end_date_pred
+        self.interval_pred = interval_pred
         self.initial_wallet = initial_wallet
+        self.initial_stock = initial_stock
+        self.invest_perc = invest_perc
         self.fees_perc = fees_perc
         self.fees_fixed = fees_fixed
-        self.forecast = forecast
+        # Initialize the calendars
+        # self.signal_calendar = np.arange(len(df), dtype=int)
+        # self.wallet_calendar = np.zeros(len(self.df))
+        # self.wallet_calendar[0] = self.initial_wallet
+        # self.stock_calendar = np.zeros(len(self.df))
+
         
-    def calcola(self, segnale):
-        # resetta per ripetere i calcoli
-        self.capitale_tempo = np.zeros(len(self.original_series))
-        self.capitale_tempo[0] = self.initial_wallet
-        self.stock = np.zeros(len(self.original_series))
+    # Could be an internal method ?
+    def calculate_signal_calendar(self):
+
+
+    def calculate_money_calendar(self): 
+        # reset to repeat calculations
         
-        investimento = self.initial_wallet
-        for t in self.tempo[self.forecast:-self.forecast]:
+        for t in self.signal_calendar:
             #print(t, end=' ')
             # se il segale non è cambiato rispetto al t precedente
-            if (segnale[t-1] ==segnale[t]):
-                self.capitale_tempo[t] = self.capitale_tempo[t-1] # il capitale rimane uguale
-                #print(self.capitale_tempo[t])
-                self.stock[t] = self.stock[t-1]  # le azioni rimangono uguali
-            else: # se il segnale è cambiato, vendi o compra
-                if segnale[t] == 1:
-                    # ha ripreso a crescere, è un minimo -> compra
-                    self.compra(t, investimento)
-                elif segnale[t] == 0:
+            if (self.signal_calendar[t-1] == self.signal_calendar[t]):
+                self.wallet_calendar[t] = self.wallet_calendar[t-1] # il capitale rimane uguale
+                #print(self.wallet_calendar[t])
+                self.stock_calendar[t] = self.stock_calendar[t-1]  # le azioni rimangono uguali
+            else: # se il signal è cambiato, sell o buy
+                if self.signal_calendar[t] == 1:
+                    # ha ripreso a crescere, è un minimo -> buy
+                    self.buy(t, self.invest_perc)
+                elif self.signal_calendar[t] == 0:
                     # sta scendendo, è un massimo -> 
-                    # vendi lo stock che hai l'istante precedente
-                    self.vendi(t, self.stock[t-1])
-                #print(self.capitale_tempo[t])
+                    # sell lo stock_calendar che hai l'istante precedente
+                    self.sell(t, self.stock_calendar[t-1])
+                #print(self.wallet_calendar[t])
+
+        # Merge calendar stock_calendar on df in order to monetize the value of the stocks 
+        #print(f"Total Wallet {} euro\nTotal Stock {} \nTotal Wallet+Stock {} euro")
                 
         # l'ultimo giorno dei dati a disposizione non lo uso perché
         # non posso verificare la previsione
         # il capitale rimane lo stesso
-        self.capitale_tempo[t+1] = self.capitale_tempo[t]
+        self.wallet_calendar[t+1] = self.wallet_calendar[t]
     
-    def compra(self, t, ammontare_euro):
-        self.capitale_tempo[t] = self.capitale_tempo[t-1] - ammontare_euro * (1 + self.fees_perc) 
-        self.stock[t] = self.stock[t-1] + ammontare_euro/self.original_series[t]
+    def buy(self, t, wallet_amount):
+        self.wallet_calendar[t] = self.wallet_calendar[t-1] - wallet_amount * (1 + self.fees_perc) 
+        self.stock_calendar[t] = self.stock_calendar[t-1] + wallet_amount/self.df[t]
         
-    def vendi(self, t, ammontare_stock):
-        self.capitale_tempo[t] = self.capitale_tempo[t-1] + ammontare_stock*self.original_series[t] * (1 - self.fees_perc)
-        self.stock[t] = self.stock[t-1] - ammontare_stock
-        
+    def sell(self, t, stock_amount):
+        self.wallet_calendar[t] = self.wallet_calendar[t-1] + stock_amount*self.df[t] * (1 - self.fees_perc)
+        self.stock_calendar[t] = self.stock_calendar[t-1] - stock_amount
+
+
+# The real program 
+
